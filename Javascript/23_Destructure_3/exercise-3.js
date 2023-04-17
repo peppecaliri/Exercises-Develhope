5b22c6d8686193d0e9838ae48a9b6db3bb22bed6
peppecaliri <giuseppecaliri95@gmail.com>
2023-04-17 18:20:14 +0200

    Exercise 23

---
+++ Javascript/23_Destructure_3/exercise-3.js
@@ -0,0 +1,12 @@
+function isAdult({ id, firstName, lastName, age } = person) {
+  return age >= 18;
+}
+
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+console.log(isAdult(person));
