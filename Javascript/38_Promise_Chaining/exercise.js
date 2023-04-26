709f35e261cab8d5a17fc649dc61ab594cbee45b
peppecaliri <giuseppecaliri95@gmail.com>
2023-04-26 17:18:08 +0200

    exercise 38

---
+++ Javascript/38_Promise_Chaining/exercise.js
@@ -0,0 +1,31 @@
+const isLogged = true;
+
+function firstPromise(result) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isLogged === true) {
+        result = Math.random();
+        resolve(result);
+      } else {
+        reject(new Error());
+      }
+    }, 500);
+  });
+}
+
+function secondPromise(result) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (result > 0.5) {
+        resolve(`{name: "John", age: 24}`);
+      } else {
+        reject(new Error("Number is smaller than 0.5"));
+      }
+    }, 1000);
+  });
+}
+
+firstPromise()
+  .then(secondPromise)
+  .then((val) => console.log(val))
+  .catch((err) => console.log(err));
