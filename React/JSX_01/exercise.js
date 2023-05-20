aa458a630461c7758ccf0d5aa16f4605752eff6c
peppecaliri <giuseppecaliri95@gmail.com>
2023-05-20 19:26:40 +0200

    exercise 1

---
+++ React/JSX_01/exercise.js
@@ -0,0 +1,10 @@
+// Create a variable called `hello` and inizialize it with an `h1` tag with the message `"Hello, World!"`.
+import React from "react";
+
+const hello = <h1>Hello, World!</h1>;
+
+class Hello extends React.Component {
+  render() {
+    return hello;
+  }
+}
