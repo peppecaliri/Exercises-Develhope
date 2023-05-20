a2a180b0cf14869e321da682f6fd679fcf56d3f4
peppecaliri <giuseppecaliri95@gmail.com>
2023-05-20 19:31:19 +0200

    exercise 2

---
+++ React/JSX_02/exercise.js
@@ -0,0 +1,4 @@
+// Write a function that receives a `name` argument and returns a `h1` tag with the `name` variable appended to the `"Hello, "` string.
+import React from "react";
+
+const PrintName = (n) => <h1>Hello, {n} </h1>;
