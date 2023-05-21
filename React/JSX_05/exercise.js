802202bae01e1709d236de5bb2bc1bee03e593fd
peppecaliri <giuseppecaliri95@gmail.com>
2023-05-21 16:22:24 +0200

    exercise 5

---
+++ React/JSX_05/exercise.js
@@ -0,0 +1,5 @@
+// Write a function that receives two parameters, `a` and `b` and returns the sum of them within an `h2` tag.
+
+import React from "react";
+
+let sum = (a, b) => <h2> {a + b} </h2>;
