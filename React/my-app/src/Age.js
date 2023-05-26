bcd5d9ecbef1b9e83167c041625c6fe2d182e4e0
peppecaliri <giuseppecaliri95@gmail.com>
2023-05-26 17:35:51 +0200

    exercise6

---
+++ React/my-app/src/Age.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Age extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Your age is {this.props.age}</p>
+      </div>
+    );
+  }
+}
+
+export default Age;
