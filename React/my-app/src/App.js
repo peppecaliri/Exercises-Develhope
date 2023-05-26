a3af9d392abd4de600bef6b9e0c7523801314680
peppecaliri <giuseppecaliri95@gmail.com>
2023-05-26 16:57:57 +0200

    finished exercise 2

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Hello from "./Hello";
+import Welcome from "./Welcome";
 
 class CreateDiv extends React.Component {
   render() {
     return (
       <div>
         <Hello />
+        <Welcome name="Peppe" />
       </div>
     );
   }
