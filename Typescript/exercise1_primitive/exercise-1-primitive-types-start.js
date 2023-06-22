e13a015c95ee462b1c6a94bf208fb44704e11003
peppecaliri <giuseppecaliri95@gmail.com>
2023-06-22 12:19:24 +0200

    exercise 1 typescript

---
+++ Typescript/exercise1_primitive/exercise-1-primitive-types-start.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+// Add type annotations to each of these variables.
+var country = "Italy";
+var year = undefined;
+var averageTemperature = 23.6;
+var visited = true;
+var currency = null;
+// Change the type annotations on these variables so they are correct.
+var population = 47450795;
+var isSummer = false;
+var languages = "Spanish, English";
