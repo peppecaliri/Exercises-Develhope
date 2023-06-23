463eb8b4fb0469f5a10105a52ffd24c3c852075b
peppecaliri <giuseppecaliri95@gmail.com>
2023-06-23 12:43:50 +0200

    exercsie 10

--- Typescript/exerrcise10_generic_interface/exercise-2-make-an-interface-generic-start.ts
+++ Typescript/exerrcise10_generic_interface/exercise-2-make-an-interface-generic-start.ts
@@ -6,21 +6,21 @@
 // This should fix the type errors on Lines 14 and 21.
 // Hint: The type for the `languages` property should use a type variable.
 
-interface Country {
-    name: string;
-    languages: unknown;
+interface Country<Type> {
+  name: string;
+  languages: Type;
 }
 
 const languagesObj1: Country<string> = {
-    name: "New Zealand",
-    languages: "English, Māori"
+  name: "New Zealand",
+  languages: "English, Māori",
 };
 
 console.log(languagesObj1.languages);
 
 const languagesObj2: Country<string[]> = {
-    name: "Spain",
-    languages: ["Spanish", "Catalan", "Galician", "Basque", "Valencian"],
+  name: "Spain",
+  languages: ["Spanish", "Catalan", "Galician", "Basque", "Valencian"],
 };
 
 console.log(languagesObj2.languages.join(", "));
