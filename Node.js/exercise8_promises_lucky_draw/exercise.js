532d22d77d62f545c9b1c4573e226803fe29de4c
peppecaliri <giuseppecaliri95@gmail.com>
2023-07-07 15:43:58 +0200

    exercise 8

---
+++ Node.js/exercise8_promises_lucky_draw/exercise.js
@@ -0,0 +1,19 @@
+const players = ["Joe", "Caroline", "Sabrina"];
+
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+players.forEach((player) =>
+  luckyDraw(player).then(console.log).catch(console.log)
+);
---
+++ Node.js/exercise8_promises_lucky_draw/exercise.md
@@ -0,0 +1,3 @@
+The luckyDraw function returns a promise. Create a promise chain where the function is called for for each of the players: Joe, Caroline and Sabrina
+
+Log out the resolved value for each promise and handle any promise rejections in the chain.
